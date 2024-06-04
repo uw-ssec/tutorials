0a8aab27bf7384ea3f43eff15f794e2bc60d2b90
Anant Mittal <anmittal@cs.washington.edu>
2024-06-04 14:35:15 -0700

    fix: Add download_all_tutorial_data to init

--- resources/src/ssec_tutorials/__init__.py
+++ resources/src/ssec_tutorials/__init__.py
@@ -1,6 +1,7 @@
 from .setup import (
     ASTROPH_ARXIV_ABSTRACTS,
     ASTROPY_GITHUB,
+    download_all_tutorial_data,
     download_astroph_arxiv_abstracts,
     download_astropy_github_documents,
     download_olmo_model,
@@ -15,13 +16,14 @@ from .setup import (
 __all__ = [
     "ASTROPH_ARXIV_ABSTRACTS",
     "ASTROPY_GITHUB",
+    "download_all_tutorial_data",
     "download_astroph_arxiv_abstracts",
     "download_astropy_github_documents",
     "download_olmo_model",
     "download_qdrant_data",
     "fetch_and_process_github_rst_files",
     "OLMO_MODEL",
-    "TUTORIAL_CACHE", 
+    "TUTORIAL_CACHE",
     "QDRANT_COLLECTION_NAME",
     "QDRANT_PATH",
 ]
