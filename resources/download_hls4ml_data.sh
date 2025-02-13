70c9e36799d871e93a683be67abd2f522e53aeaa
Niki Burggraf <nikib@uw.edu>
2025-02-13 18:00:36 +0000

    add everything

--- resources/download_hls4ml_data.sh
+++ resources/download_hls4ml_data.sh
@@ -1,3 +1,3 @@
 pip install --upgrade --quiet  beautifulsoup4
 
-!wget -r -A.html -P rtdocs https://fastmachinelearning.org/hls4ml/
+wget -r -A.html -P rtdocs https://fastmachinelearning.org/hls4ml/
